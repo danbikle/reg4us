51790923d7def6e8a7f268cde42f0538741ef101
Dan Bikle <danbikle@outlook.com>
2018-12-29 14:26:12 -0800

    more

---
+++ test/system/browse_links1s_test.rb
@@ -0,0 +1,16 @@
+# browse_links1s_test.rb
+
+# This script should help me run a system test.
+
+# Demo shell command:
+# bin/rails test:system
+
+require "application_system_test_case"
+
+class BrowseLinks1sTest < ApplicationSystemTestCase
+  test "visiting the index" do
+    visit pages_about_url
+  
+    assert_selector "h1", text: "Pages#about"
+  end
+end
